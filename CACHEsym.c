0f09e79e35b47e6ccf0967898ae9766ea26fd1cd
Jorge <jorge.caseda.baroja@gmail.com>
2020-12-13 20:03:00 +0100

    Funcion de ller fichero binario terminada. Funcion de leer y descomponer los números hexadecimales a medias. Falta las comparaciones y el sleep

--- CACHEsym.c
+++ CACHEsym.c
@@ -30,20 +30,46 @@ void readFBinary(){
         exit(-1);
     }
     fread(RAM, sizeof(RAM), 1, fRAM);
-    printf("caracter: %d\n", RAM[499]);
+    printf("caracter: %d\n", RAM[216]);
     fclose(fRAM);
 }
 
+//Método para leer y descomponer los números hexadecimales
 void readFHexadecimal(){
     FILE *fichero;
-    unsigned int num[12];
+    unsigned int hexNum[12];
+    short int etiqueta;
+    short int palabra;
+    short int linea;
+    short int bloque;
+    char texto[100];
     fichero = fopen("accesos_memoria.txt", "r");
     if (fichero == NULL){
         printf("El fichero no existe\n");
         exit(-1);
     }
-    fscanf(fichero, "%X", num);
-    printf("Numero %d\n", num[0]);
+
+    fscanf(fichero, "%X", &hexNum[0]);
+    linea = (hexNum[0] & 0b0000011000) >> 3;
+    palabra = (hexNum[0] & 0b0000000111);
+    etiqueta = hexNum[0] >> 5;
+    bloque = hexNum[0] >> 3;
+    printf("Linea %d\n", linea);
+    printf("Palabra %d\n", palabra);
+    printf("Etiqueta %d\n", etiqueta);
+    printf("Bloque %d\n", bloque);
+    printf("Numero %x\n", hexNum[0]); 
+
+    if (cache[0].ETQ == etiqueta){
+        printf("T: %d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X", tiempoGlobal, hexNum[0], etiqueta, linea, palabra, tiempoGlobal);
+        tiempoGlobal += 1;
+    }
+    else {
+        numFallos++;
+        printf("T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X", tiempoGlobal, numFallos, hexNum[0], etiqueta, linea, palabra, bloque);
+        tiempoGlobal += 10;
+
+    }
     
     fclose(fichero);
 }
