1f54cb96183c65a21b174e4816bac4da1cc27d35
Jorge <jorge.caseda.baroja@gmail.com>
2020-12-13 23:18:01 +0100

    Funcion comprobacion terminada. Sleep y 4 funciones realizadas.

--- CACHEsym.c
+++ CACHEsym.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <unistd.h>
 
 typedef struct {
     short int ETQ;
@@ -9,6 +10,8 @@ typedef struct {
 T_LINEA_CACHE cache[4];
 int tiempoGlobal = 0;
 int numFallos = 0;
+int numAccesos = 0;
+unsigned char RAM[1024];
 
 //Metodo que inicializa todas los campos de la cache
 void inicializar(){
@@ -22,7 +25,6 @@ void inicializar(){
 
 //Metodo para leer el fichero binario RAM.bin
 void readFBinary(){
-    unsigned char RAM[1024];
     FILE *fRAM;
     fRAM = fopen("RAM.bin", "rb");
     if (fRAM == NULL){
@@ -30,11 +32,21 @@ void readFBinary(){
         exit(-1);
     }
     fread(RAM, sizeof(RAM), 1, fRAM);
-    printf("caracter: %d\n", RAM[216]);
     fclose(fRAM);
 }
 
-//Método para leer y descomponer los números hexadecimales
+//Metodo para imprimir los datos de la cache
+void imprimirDatos(){
+    for (int i = 0; i < 4; i++){
+            printf("ETQ:%X   Datos ", cache[i].ETQ);
+            for (int j = 7; j >= 0; j--){
+                printf("%X ", cache[i].Datos[j]);
+            }
+            printf("\n");
+    }
+}
+
+//Método para leer y descomponer los números hexadecimales para luego comprobar si la etiqueta de la direcion es igual a la de la linea de la cache
 void readFHexadecimal(){
     FILE *fichero;
     unsigned int hexNum[12];
@@ -43,35 +55,52 @@ void readFHexadecimal(){
     short int linea;
     short int bloque;
     char texto[100];
+    int contador = 0;
     fichero = fopen("accesos_memoria.txt", "r");
     if (fichero == NULL){
         printf("El fichero no existe\n");
         exit(-1);
     }
 
-    fscanf(fichero, "%X", &hexNum[0]);
-    linea = (hexNum[0] & 0b0000011000) >> 3;
-    palabra = (hexNum[0] & 0b0000000111);
-    etiqueta = hexNum[0] >> 5;
-    bloque = hexNum[0] >> 3;
-    printf("Linea %d\n", linea);
-    printf("Palabra %d\n", palabra);
-    printf("Etiqueta %d\n", etiqueta);
-    printf("Bloque %d\n", bloque);
-    printf("Numero %x\n", hexNum[0]); 
+    for (int i = 0; i < 12; i++){
+        fscanf(fichero, "%X", &hexNum[i]);
+        linea = (hexNum[i] & 0b0000011000) >> 3;
+        palabra = (hexNum[i] & 0b0000000111);
+        etiqueta = hexNum[i] >> 5;
+        bloque = hexNum[i] >> 3;
 
-    if (cache[0].ETQ == etiqueta){
-        printf("T: %d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X", tiempoGlobal, hexNum[0], etiqueta, linea, palabra, tiempoGlobal);
-        tiempoGlobal += 1;
-    }
-    else {
-        numFallos++;
-        printf("T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X", tiempoGlobal, numFallos, hexNum[0], etiqueta, linea, palabra, bloque);
-        tiempoGlobal += 10;
+        //Comprobacion de si la etiqueta de la direcion es igual a la de la linea de la cache
+        if (cache[linea].ETQ != etiqueta) {
+            numFallos++;
+            printf("T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X\n", tiempoGlobal, numFallos, hexNum[i], etiqueta, linea, palabra, bloque);
+            tiempoGlobal += 10;
+            cache[linea].ETQ = etiqueta;
+            //Se copia el bloque correspondiente desde el array RAM
+            for (int j = 0; j < 8; j++){
+                cache[linea].Datos[j] = RAM[bloque * 8 + j];
+            }
+            printf("Cargando el bloque %02X en la linea %02X\n", bloque, linea);
 
+        }
+        if (cache[linea].ETQ == etiqueta){
+            numAccesos++;
+            tiempoGlobal += 1;
+            printf("T: %d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X\n", tiempoGlobal, hexNum[i], etiqueta, linea, palabra, RAM[bloque *8 + palabra]);
+            texto[contador+1] = RAM[bloque *8 + palabra];
+            contador++;
+        }
+        
+        imprimirDatos();
+        sleep(2);
+        printf("\n");
     }
-    
+    printf("\nNúmero total de accesos: %d\nNúmero de fallos: %d\nTexto leido: ", numAccesos, numFallos);
+    for (int j = 0; texto[j] != '\0'; j++){
+        printf("%c", texto[j]);
+    }
+    printf("\n");
     fclose(fichero);
+    exit(-1);
 }
 
 void main(int argc, char *argv[])
---
+++ README.txt
@@ -0,0 +1,1 @@
+Jorge Cáseda Baroja y Álvaro Segura Simón
